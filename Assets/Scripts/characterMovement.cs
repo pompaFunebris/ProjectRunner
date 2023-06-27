c75d98b241d1a614455ecff13e0f36cab83761c1
mpuczynski55 <72734576+mpuczynski55@users.noreply.github.com>
2023-06-27 23:12:36 +0200

    menu

--- Assets/Scripts/characterMovement.cs
+++ Assets/Scripts/characterMovement.cs
@@ -2,20 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class characterMovement : MonoBehaviour
 {
     private Rigidbody rb;
-    //private GameObject gameObject;
     public levelCharacteristics characteristics;
     private float jumpMultiplier;
     private float speed;
     private float songBPM;
     private bool canJump = false;
     public GameObject objectCharacteristics;
-    public int points;
-    private float jumpOffset;
 
     private void OnCollisionEnter(Collision collision) {
         canJump = true;
@@ -31,8 +27,6 @@ public class characterMovement : MonoBehaviour
         jumpMultiplier = characteristics.jumpMultiplier;
         speed = characteristics.speed;
         songBPM = characteristics.songBPM;
-        jumpOffset = characteristics.jumpOffset;
-
     }
 
     // Update is called once per frame
@@ -42,16 +36,7 @@ public class characterMovement : MonoBehaviour
             //Debug.Log(canJump);
             if (touch.phase == 0) {
                 if (transform.position.y < 0.1f) {
-                    GetComponent<Animator>().Play("JumpWhileRunning");
-                    GameObject[] gos = GameObject.FindGameObjectsWithTag("obstacle");
-                    float scoreOffset = Mathf.Infinity;
-                    foreach (GameObject go in gos) {
-                        if (Math.Abs(scoreOffset) > Math.Abs(go.transform.position.z - jumpOffset - transform.position.z)) scoreOffset = go.transform.position.z - jumpOffset - transform.position.z;
-                    }
-                    Debug.Log(scoreOffset);
-
-                    //rb.GetComponent<Animator>().Play("JumpWhileRunning");
-                    //transform.position = transform.position + new Vector3(0, jumpMultiplier, 0);
+                    transform.position = transform.position + new Vector3(0, jumpMultiplier, 0);
                 }
             }
         }
